1a527d158318aa135fe20518c954c5435b904e21
Eduardo Maulén <ed.maulen@duocuc.cl>
2025-12-04 14:56:05 -0300

    Agrega lint test al ci

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,12 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    '**/*.test.js',
+    '**/*.test.jsx',
+    '**/*.test.ts',
+    '**/*.test.tsx',]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import './App.css'
 import Contador from './components/Contador'
 
 const App = () => {
+  const func = () => {
+    console.log("hiola");
+  }
+
   return (
     <>
       <Contador />
